7c6cf8cf072fbc2933a84e38c8448b97bb783a18
RV <valuhar@gmail.com>
2022-12-05 19:31:31 +0530

    guess2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,19 @@
 use std::io;
-//   The io library comes from the standard library.
+use std::cmp::Ordering;
+//   The io and cmp library comes from the standard library.
 //   Rust has a set of items defined in the standard library.
-//   This set is called the prelude. 
+//   This set is called the prelude.
+
+use rand::Rng; 
+//   For random num generation. 
+//   rand is dep that we need get from Crate. 
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -23,4 +32,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => println!("You Win!!!"),
+    }
 }
